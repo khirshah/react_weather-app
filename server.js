adfe07b06b6ccd7f23bfcf093a85532db481f040
khirshah <khirshah@gmail.com>
2019-09-27 16:44:48 +0100

    Procfiles and server.js added

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 3000;
+const app = express();
+
+// the __dirname is the current directory from where the script is running
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// send the user to index html page inspite of the url
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(port, () => console.log(`App is listening on port ${port}`));
